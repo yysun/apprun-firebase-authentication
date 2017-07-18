c094a0a9b86d747175ba27a3d6d6211369052f34
Yiyi Sun <yiyisun@gmail.com>
2017-07-18 18:58:28 -0400

    remove header

--- main.tsx
+++ main.tsx
@@ -2,7 +2,6 @@ import app, { Component } from 'apprun';
 import home from './home';
 import about from './about';
 import contact from './contact';
-import Header from './header';
 import signin from './signin';
 
 const app_id = 'my-app';
@@ -23,7 +22,27 @@ app.on('//', route => {
 class AppComponent extends Component {
 
   view = state => <div>
-    <Header />
+    <nav className="navbar navbar-default navbar-static-top">
+      <div className="container">
+        <div className="navbar-header">
+          <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false"
+            aria-controls="navbar">
+            <span className="sr-only">Toggle navigation</span>
+            <span className="icon-bar"></span>
+            <span className="icon-bar"></span>
+            <span className="icon-bar"></span>
+          </button>
+          <a className="navbar-brand" href="#/">Project name</a>
+        </div>
+        <div id="navbar" className="collapse navbar-collapse">
+          <ul className="nav navbar-nav">
+            <li className="active"><a href="#">Home</a></li>
+            <li><a href="#about">About</a></li>
+            <li><a href="#contact">Contact</a></li>
+          </ul>
+        </div>
+      </div>
+    </nav>
     <div className="container" id="main"></div>
   </div>
 
