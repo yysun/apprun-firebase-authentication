d3fe4a5f011255af8c7d17c3fa1caafa1e535516
Yiyi Sun <yiyisun@gmail.com>
2018-03-04 09:09:34 -0500

    upgrade apprun and webpack

--- webpack.config.js
+++ webpack.config.js
@@ -4,17 +4,19 @@ module.exports = {
     'app': './main.tsx',
   },
   output: {
-    filename: '[name].js'
+    filename: '[name].js',
+    path: path.resolve(__dirname)
   },
   resolve: {
-    extensions: ['.webpack.js', '.web.js', '.ts', '.tsx', '.js']
+    extensions: ['.ts', '.tsx', '.js']
   },
   module: {
     rules: [
       { test: /.tsx?$/, loader: 'ts-loader' }
     ]
   },
-  devtool: "source-map",
   devServer: {
-  }
+    open: true
+  },
+  devtool: 'source-map'
 }
